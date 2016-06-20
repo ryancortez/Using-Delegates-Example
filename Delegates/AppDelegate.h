f1a317ae1155a50db9e60fbccb29458dcc550b56
ryancortez <ryancortez@mac.com>
2016-06-20 12:09:04 -0500

    Initial Commit

---
+++ Delegates/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Delegates
+//
+//  Created by Ryan Cortez on 6/20/16.
+//  Copyright © 2016 Ryan Cortez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Delegates/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Delegates
+//
+//  Created by Ryan Cortez on 6/20/16.
+//  Copyright © 2016 Ryan Cortez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
